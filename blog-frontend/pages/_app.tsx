f6e5923c81932d4e874544182e4a2b9255596ad7
mubinkhan17102 <mubin17102@gmail.com>
2022-08-26 15:50:23 +0600

    creting the main module

--- blog-frontend/pages/_app.tsx
+++ blog-frontend/pages/_app.tsx
@@ -2,7 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <div className='container mx-auto font-sans'>
+        <nav>This is our nav</nav>
+        <main>
+          <Component {...pageProps}/>
+        </main>
+        <footer>This is our footer</footer>
+      </div>
+    </>
+  ) 
 }
 
 export default MyApp
